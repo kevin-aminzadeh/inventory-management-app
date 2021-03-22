5304bbd1e2a3b7354fbe534e62bc2d8c1b32b117
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-22 21:10:58 +1030

    feat: Update seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,17 @@
 const sequelize = require('../config/connection');
-const { UserModel, RoleModel } = require('../models');
+const {
+  UserModel,
+  RoleModel,
+  BrandModel,
+  CategoryModel,
+  ProductModel,
+} = require('../models');
 
 const userData = require('./userData.json');
 const roleData = require('./roleData.json');
+const brandData = require('./brandData.json');
+const categoryData = require('./categoryData.json');
+const productData = require('./productData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -15,6 +24,23 @@ const seedDatabase = async () => {
 
   // Seed The User Table
   await UserModel.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  // Seed the brand table
+  await BrandModel.bulkCreate(brandData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  // Seed the Category table
+  await CategoryModel.bulkCreate(categoryData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await ProductModel.bulkCreate(productData, {
     individualHooks: true,
     returning: true,
   });
