415ef8ac1862e6f756ece0042226201a3e9e4c0a
Grace Palenzuela <gracee_palenzuela@yahoo.com>
2021-03-22 20:45:09 +1030

    feat: admin can update user info

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,3 +17,29 @@ exports.getUsers = async (req, res, next) => {
     return res.status(400).json({ status: 400, message: err.message });
   }
 };
+exports.updateUser = async (req, res, next) => {
+  // Validate Request Parameters/Queries
+
+  const userId = req.params.id;
+  if (req.body.name || req.body.email || req.body.password || req.body.role_id) {
+    const updatedData = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      role_id: req.body.role_id
+    }
+    try {
+      const user = await UserService.updateUser({ ...updatedData }, userId);
+      return res.status(200).json({
+        status: 200,
+        data: user,
+        message: 'Successfully Updates User.',
+      });
+    } catch (err) {
+      return res.status(400).json({ status: 400, message: err.message });
+    }
+  } 
+  else {
+    res.status(400).json({ status: 400, message: 'Employee Not Found' });
+  };
+};
--- routes/api/user.routes.js
+++ routes/api/user.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const UserController = require('../../controllers/user.controller');
+const { updateUsers } = require('../../services/user.service');
+
 
 router.get('/', UserController.getUsers);
-
+router.put('/:id',UserController.updateUser);
 module.exports = router;
