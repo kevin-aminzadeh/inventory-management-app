6c9ea034d134486f97565a2b854ff5e64eed185f
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-22 21:10:34 +1030

    feat: Update index junction.

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const UserModel = require('./user.model');
 const RoleModel = require('./role.model');
 const ProductModel = require('./product.model');
+const ProductItemModel = require('./productItem.model');
+const BrandModel = require('./brand.model');
+const CategoryModel = require('./category.model');
 
-module.exports = { UserModel, RoleModel, ProductModel };
+module.exports = {
+  UserModel,
+  RoleModel,
+  ProductModel,
+  ProductItemModel,
+  BrandModel,
+  CategoryModel,
+};
