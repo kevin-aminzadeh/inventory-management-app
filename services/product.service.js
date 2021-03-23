1266099b225662b65200e66571a9aefbc86c1945
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-23 20:38:10 +1030

    feat: Created product services

---
+++ services/product.service.js
@@ -0,0 +1,29 @@
+const { ProductModel, ProductItemModel } = require('../models/index');
+
+exports.getProducts = async () => {
+  try {
+    const products = await ProductModel.findAll({
+      include: {
+        model: ProductItemModel,
+      },
+    });
+    return products;
+  } catch (err) {
+    // Log Errors
+    throw Error(err);
+  }
+};
+
+exports.getProductById = async (id) => {
+  try {
+    const products = await ProductModel.findByPk(id, {
+      include: {
+        model: ProductItemModel,
+      },
+    });
+    return products;
+  } catch (err) {
+    // Log Errors
+    throw Error(err);
+  }
+};
