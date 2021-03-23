1d28a95fb8981aeeb60a66ef34780c5351285520
Grace Palenzuela <gracee_palenzuela@yahoo.com>
2021-03-23 20:34:30 +1030

    feat: search user by name and email

--- routes/api/user.routes.js
+++ routes/api/user.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const UserController = require('../../controllers/user.controller');
+const UserController = require('../../services/user.service');
 const { updateUsers } = require('../../services/user.service');
 
 
 router.get('/', UserController.getUsers);
+router.get('/search/',UserController.getUser);
 router.put('/:id',UserController.updateUser);
 module.exports = router;
--- services/user.service.js
+++ services/user.service.js
@@ -9,18 +9,37 @@ exports.getUsers = async (query, page, limit) => {
     throw Error(err);
   }
 };
+exports.getUser = async (req, res) => {
+  try {
+    if (req.body.name) {
+      const userData = await User.findAll({
+        where: { name: req.body.name },
+      });
+      res.json(userData);
+    } else {
+      const userData = await User.findAll({
+        where: { email: req.body.email },
+      });
+      res.json(userData);
+    }
+  } catch (err) {
+    // Log Errors
+    throw Error(err);
+  }
+};
+
 exports.updateUser = async (query, userId) => {
   try {
     const userData = await User.findByPk(userId);
-    if(userData){
-      try{
-        User.update(query,{
-          where: {id: `${userId}`},
+    if (userData) {
+      try {
+        User.update(query, {
+          where: { id: `${userId}` },
         });
-      }catch(err){
+      } catch (err) {
         throw Error(err);
-      };
-    };
+      }
+    }
   } catch (err) {
     // Log Errors
     throw Error(err);
