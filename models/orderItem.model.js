c047f20546775587656bfb713e2ec517e93ef4bd
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-26 22:40:13 +1030

    feat: Created OrderItem Model

---
+++ models/orderItem.model.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class OrderItem extends Model {}
+
+OrderItem.init(
+  {
+    item_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      references: {
+        model: 'item',
+        key: 'id',
+      },
+    },
+    order_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      references: {
+        model: 'order',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'orderItem',
+  }
+);
+
+module.exports = OrderItem;
