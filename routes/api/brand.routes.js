9934d7f980961e807ee40487ee9b3f8903aa2cee
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-26 20:32:29 +1030

    feat: added post and delete routes to the brand.routes api

--- routes/api/brand.routes.js
+++ routes/api/brand.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const BrandController = require('../../controllers/brand.controller');
 
-router.get('/:id',  BrandController.searchBrand);
-
+router.get('/:id', BrandController.searchBrand);
+router.post('/', BrandController.addBrand)
+router.delete('/:id',BrandController.deleteBrand)
 module.exports = router;
