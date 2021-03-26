f67f80f089dd88f87b0090d1c4f490a90b7ae6cc
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-26 20:32:03 +1030

    feat: added AddProduct && Delete produyct to the controllers

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -25,3 +25,27 @@ exports.getProductById = async (req, res, next) => {
     return res.status(400).json({ status: 400, message: err.message });
   }
 };
+exports.addProduct = async (req, res, next) => {
+  try {
+    const products = await ProductController.addProduct(req.body);
+    return res.status(200).json({
+      status: 200,
+      data: products,
+      message: 'Successfully added product.',
+    });
+  } catch (err) {
+    return res.status(400).json({ status: 400, message: err.message });
+  }
+};
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    const products = await ProductController.deleteProduct(req.params.id);
+    return res.status(200).json({
+      status: 200,
+      data: products,
+      message: 'Successfully Retrieved product.',
+    });
+  } catch (err) {
+    return res.status(400).json({ status: 400, message: err.message });
+  }
+};
