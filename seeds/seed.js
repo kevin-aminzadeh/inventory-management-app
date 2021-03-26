18a1730e1d84b7caf4f703124e65f4fefed95960
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-26 22:41:31 +1030

    chore: updated seed File

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,15 @@
 const sequelize = require('../config/connection');
-const { User, Role, Brand, Category, Product, Item } = require('../models');
+const {
+  User,
+  Role,
+  Brand,
+  Category,
+  Product,
+  Item,
+  OrderStatus,
+  Order,
+  OrderItem,
+} = require('../models');
 
 const userData = require('./userData.json');
 const roleData = require('./roleData.json');
@@ -7,6 +17,9 @@ const brandData = require('./brandData.json');
 const categoryData = require('./categoryData.json');
 const productData = require('./productData.json');
 const itemData = require('./itemData.json');
+const orderData = require('./orderData.json');
+const orderStatusData = require('./orderStatusData.json');
+const orderItemData = require('./orderItemData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -34,17 +47,36 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
+
   // Seed the product table
   await Product.bulkCreate(productData, {
     individualHooks: true,
     returning: true,
   });
 
+  // Seed The Item Table
   await Item.bulkCreate(itemData, {
     individualHooks: true,
     returning: true,
   });
 
+  // Seed The Order Status Table
+  await OrderStatus.bulkCreate(orderStatusData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  // Seed The Order Table
+  await Order.bulkCreate(orderData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  // Seed The OrderItem
+  await OrderItem.bulkCreate(orderItemData, {
+    individualHooks: true,
+    returning: true,
+  });
   process.exit(0);
 };
 
