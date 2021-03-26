17b591a2fac77dd9df3a55c844fae9e5720c5d0a
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-26 20:33:37 +1030

    feat: added post && deletes routes to the product.routes api

--- routes/api/product.routes.js
+++ routes/api/product.routes.js
@@ -3,5 +3,7 @@ const ProductController = require('../../controllers/product.controller');
 
 router.get('/', ProductController.getProduct);
 router.get('/:id', ProductController.getProductById);
+router.post('/', ProductController.addProduct);
+router.delete('/:id', ProductController.deleteProduct);
 
 module.exports = router;
