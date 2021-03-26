a65de6db7a01142faa30f03a347b9515db9eeadf
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-26 20:34:00 +1030

    feat: added addBrand && delete brand service

--- services/brand.service.js
+++ services/brand.service.js
@@ -16,3 +16,27 @@ exports.searchBrands = async (param) => {
     throw Error(err);
   }
 };
+
+exports.addBrand = async (param) => {
+  try {
+    const products = await Brand.create(param);
+    return products;
+  } catch (err) {
+    // Log Errors
+    throw Error(err);
+  }
+};
+
+exports.deleteBrand = async (param) => {
+  try {
+    const products = await Brand.destroy({
+      where: {
+        id: param,
+      },
+    });
+    return products;
+  } catch (err) {
+    // Log Errors
+    throw Error(err);
+  }
+};
