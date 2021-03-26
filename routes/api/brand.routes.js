a401141c98b88fbf0b3ed42807d08e6478f8f7ba
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-26 21:12:08 +1030

    feat: update brand routes

--- routes/api/brand.routes.js
+++ routes/api/brand.routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const BrandController = require('../../controllers/brand.controller');
 
 router.get('/:id', BrandController.searchBrand);
-router.post('/', BrandController.addBrand)
-router.delete('/:id',BrandController.deleteBrand)
+router.post('/', BrandController.addBrand);
+router.delete('/:id', BrandController.deleteBrand);
+router.patch('/:id', BrandController.updateBrand);
 module.exports = router;
