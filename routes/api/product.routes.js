1e68645bca4586e6cbd4c48e3b96d217b1cf51ad
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-26 15:17:37 +1030

    feat: added AddProduct routes

--- routes/api/product.routes.js
+++ routes/api/product.routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const ProductController = require('../../controllers/product.controller');
+const ProductItemController = require('../../controllers');
 
 router.get('/', ProductController.getProduct);
 router.get('/:id', ProductController.getProductById);
+router.put('/', ProductController.AddProduct);
 
 module.exports = router;
