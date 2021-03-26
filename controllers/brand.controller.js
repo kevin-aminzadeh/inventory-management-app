a3c0d8c473d54496d3ed542611dc5201709e0ae5
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-26 20:30:29 +1030

    feat: AddBrand && DeleteBrand

--- controllers/brand.controller.js
+++ controllers/brand.controller.js
@@ -12,3 +12,29 @@ exports.searchBrand = async (req, res, next) => {
     return res.status(400).json({ status: 400, message: err.message });
   }
 };
+
+exports.addBrand = async (req, res, next) => {
+  try {
+    const products = await BrandService.addBrand(req.body);
+    return res.status(200).json({
+      status: 200,
+      data: products,
+      message: 'Successfully Retrieved Brand.',
+    });
+  } catch (err) {
+    return res.status(400).json({ status: 400, message: err.message });
+  }
+};
+
+exports.deleteBrand = async (req, res, next) => {
+  try {
+    const products = await BrandService.deleteBrand(req.params.id);
+    return res.status(200).json({
+      status: 200,
+      data: products,
+      message: 'Successfully removed Brand.',
+    });
+  } catch (err) {
+    return res.status(400).json({ status: 400, message: err.message });
+  }
+};
