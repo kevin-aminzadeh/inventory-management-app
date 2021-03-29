7b92aec1530ba4bbacb8da87f0719f7e7c4d76f7
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-03-29 17:25:12 +1030

    fix: Fix misnamed products route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 // Initialize API Child Routes
-const productRoutes = require('./product.routes')
+const productRoutes = require('./product.routes');
 const authRoutes = require('./auth/auth.routes');
 const userRoutes = require('./user.routes');
 const brandRoutes = require('./brand.routes');
@@ -9,7 +9,7 @@ const brandRoutes = require('./brand.routes');
 // Configure API Child Routes
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
-router.use('/product', productRoutes);
+router.use('/products', productRoutes);
 router.use('/brands', brandRoutes);
 
 module.exports = router;
