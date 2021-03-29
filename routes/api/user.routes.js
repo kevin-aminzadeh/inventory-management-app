1f2f735c435e44c86aba877d8d6940e2634378ea
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-29 20:43:45 +1030

    feat: products handlebars

--- routes/api/user.routes.js
+++ routes/api/user.routes.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 const UserController = require('../../services/user.service');
 const { updateUsers } = require('../../services/user.service');
 
-
 router.get('/', UserController.getUsers);
-router.get('/search/',UserController.getUser);
-router.put('/:id',UserController.updateUser);
+router.get('/search/', UserController.getUser);
+router.put('/:id', UserController.updateUser);
 module.exports = router;
