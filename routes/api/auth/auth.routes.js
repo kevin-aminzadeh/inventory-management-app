28b608f034d33809cb35ac5493f2ed84f4647f83
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-30 17:16:38 +1030

    feat: auth

--- routes/api/auth/auth.routes.js
+++ routes/api/auth/auth.routes.js
@@ -1,3 +1,44 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../../../models/user.model');
+
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { email: req.body.email },
+    });
+    if (!userData) {
+      // eslint-disable-next-line prettier/prettier
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    const validPassword = await userData.checkPassword(req.body.password);
+    console.log(validPassword);
+    if (!validPassword) {
+      // eslint-disable-next-line prettier/prettier
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    console.log('made it here 1');
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
