1981fc15cd72d923c9656dd4c67c9c7c783b5c50
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-03-30 17:16:55 +1030

    feat: auth

--- routes/dashboard/settings.routes.js
+++ routes/dashboard/settings.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
   res.render('dashboard/settings', {
     layout: 'dashboard',
     currentRoute: req.baseUrl,
